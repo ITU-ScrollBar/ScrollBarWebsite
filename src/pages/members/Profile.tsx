0ddc8b5b55a3e3d97323bcebb59f5807732a6400
ThomasITU <thwr@berkeley.edu>
2025-10-05 21:43:33 +0200

    feat: add studyline picker to the profile

--- src/pages/members/Profile.tsx
+++ src/pages/members/Profile.tsx
@@ -1,5 +1,6 @@
 import { Path, useNavigate } from "react-router-dom";
 import { ProfileOutlined, HomeOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 import { Layout, Menu } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
@@ -7,14 +8,14 @@ import Sider from "antd/es/layout/Sider";
 import Title from "antd/es/typography/Title";
 import { useAuth } from "../../contexts/AuthContext";
 import newbiehatSvg from "../../assets/images/newbiehat.svg";
-
-import { couldStartTrivia } from "typescript";
-
+import { StudyLine } from "../../types/types-file";
+import StudyLinePicker from "./StudyLinePicker";
 
 export default function Profile() {
   const navigation = useNavigate();
   const navigateToLink = (location: Path) => navigation(location);
   const { loading, currentUser } = useAuth();
+  const [studyLine, setStudyLine] = useState<StudyLine | null>(null);
   
   // TODO
   type UserProfile = {
@@ -27,6 +28,7 @@ export default function Profile() {
     active: boolean;
     photoUrl: string;
     memberSince: number;
+    totalShifts: number;
   }
 
 
@@ -34,21 +36,23 @@ export default function Profile() {
     return <div>Loading...</div>;
   }
   console.log(currentUser)
+  
 
-    const userProfile: UserProfile | null = currentUser
-    ? {
-        displayName: currentUser.displayName ?? "Lorem",
-        email: currentUser.email ?? "test",
-        studyline: "CS", // populate from your DB/profile store if available
-        isAdmin: false, // determine from your app's authorization data
-        roles: ["tester"], // populate from your DB/profile store if available
-        phone: (currentUser as any).phoneNumber ?? null,
-        active: true,
-        photoUrl: (currentUser as any).photoURL ?? newbiehatSvg,
-        memberSince: 2004,
-      }
-    : null;
-
+  const userProfile: UserProfile | null = currentUser
+  ? {
+      displayName: currentUser.displayName ?? "Lorem",
+      email: currentUser.email ?? "test",
+      studyline: studyLine?.name ?? "Not selected", 
+      isAdmin: false, 
+      roles: ["tester"], // populate from your DB/profile store if available
+      phone: (currentUser as any).phoneNumber ?? null,
+      active: true,
+      photoUrl: (currentUser as any).photoURL ?? newbiehatSvg,
+      memberSince: 2004,
+      totalshifts: 5,
+    }
+  : null;
+  
   return(
     <Layout
       style={{
@@ -101,6 +105,10 @@ export default function Profile() {
               </div>
               <div style={{ textAlign: "left" }}>
               <h4 style={{ margin: 0 }}>{userProfile?.displayName}</h4>
+              <StudyLinePicker 
+                value={studyLine}
+                onChange={setStudyLine}
+              />
               </div>
             </div>
 
@@ -108,10 +116,10 @@ export default function Profile() {
             <h4 style={{ margin: 0, width: "100%", textAlign: "left" }}>Role: {(userProfile?.roles ?? []).join(", ")}</h4>
 
             <h4 style={{ marginTop: 8, marginBottom: 0, width: "100%", textAlign: "left" }}><b>Your Data</b></h4>
-            <h4 style={{ margin: 0, width: "100%", textAlign: "left" }}>Total shifts: {userProfile?.roles?.[0]}</h4>
+            <h4 style={{ margin: 0, width: "100%", textAlign: "left" }}>Total shifts: {userProfile?.totalshifts ?? 5 }</h4>
             <h4 style={{ margin: 0, width: "100%", textAlign: "left" }}>Member since: {userProfile?.memberSince}</h4>
 
-            <h4 style={{ marginTop: 8, marginBottom: 0, width: "100%", textAlign: "left" }}><b>Badges: TBD</b></h4>
+            <h4 style={{ marginTop: 8, marginBottom: 0, width: "100%", textAlign: "left" }}><b>Badges TBD</b></h4>
             </div>
             </Content>
           </Layout>
