892ac5115b8b4a783a778dd70140215657bd4651
ThomasITU <thwr@berkeley.edu>
2025-10-20 03:30:07 +0200

    Add event page and countdown timer for next event

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -1,57 +1,90 @@
-import React from 'react'
 
-import { Button, Col, Divider, Image, Layout, Row, Space } from 'antd'
+import { Col, Divider, Layout, Row, Card, Typography } from 'antd'
 
 import Title from 'antd/es/typography/Title';
 import Paragraph from 'antd/es/typography/Paragraph';
 import { Header } from 'antd/es/layout/layout';
 import HeaderBar from '../components/HomePage/HeaderBar';
+import CountDown from '../components/EventCountDown';
+import './EventsPage.css';
+import useEvents from '../hooks/useEvents';
 
+
+const { Text } = Typography;
 
 export default function EventsPage() {
+    const { eventState } = useEvents();
+
+    const DEFAULT_EVENT_IMAGE = "/assets/background-DD5GamNA.png";
+    const EVENT_INFORMATION_LABEL = "Get all the latest Information on the Facebook Event"
+    const DEFAULT_FACEBOOK_LINK = "https://www.facebook.com/ScrollBar"
+    const events = eventState.isLoaded ? eventState.events
+        .filter(event => event.published && !event.internal)
+        .map(event => ({
+            title: event.displayName,
+            image: event.picture ?? DEFAULT_EVENT_IMAGE,
+            start: event.start,
+            event_url: event.facebook_link ?? DEFAULT_FACEBOOK_LINK,
+        })).sort((a, b) => a.start.getTime() - b.start.getTime()) : [];
+
+    // Get the next upcoming event for countdown
+    const nextEvent = events.length > 0 ? events[0] : null;
+
   return (
-    <Layout style={{ minHeight: '100vh', minWidth: '100vw', flexDirection: 'column', height: 'auto'}}>
-       <Header
-        style={{
-          position: 'absolute',
-          top: 0,
-          width: '100%',
-          backgroundColor: 'transparent',
-          boxShadow: 'none',
-          zIndex: 3,
-          display: 'flex',
-          alignItems: 'center',
-          color: '#fff',
-        }}
-      >
+    <Layout className="eventsPage">
+       <Header className="header">
         <HeaderBar />
       </Header>
-    <Image style={{width: '100vw'}} preview={false} ></Image>
-    <Row justify="center">
+
+    <div className="heroImage" style={{ backgroundImage: `url(${nextEvent ? nextEvent.image : DEFAULT_EVENT_IMAGE})` }}>
+      {nextEvent && <CountDown nextEvent={nextEvent} />}
+    </div>    <Row justify="center">
           <Col
             md={24}
-            lg={12}
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
+            lg={20}
+            className="eventsContainer"
           >
-            <Title id="about" level={2} style={{ scrollMarginTop: '135px' }}>
-              What is ScrollBar?
+            <Title level={2} className="eventsTitle">
+              Our Events This Semester
             </Title>
-            <Paragraph style={{ fontSize: '18px', lineHeight: '36px', textAlign: 'center' }}>
-              ScrollBar is a student-run Friday bar at the IT University of Copenhagen, organized by our dedicated team of volunteer students. 
-              Founded in 2004, our goal is to bring ITU students together in a cozy and welcoming atmosphere every Friday in the semester from 3PM to 2AM. 
-              <br></br>
-              <br></br>
-              We regularly host DJs and organize a wide variety of events throughout the semester, including Birthday Parties, Back-to-School,
-              Beerpong Tournaments, Halloween parties, Hand-In celebrations and more. We’re especially proud of our wide selection of beers and drinks, 
-              but there’s something for everyone.
-              <br></br>
-              <br></br>
-              This covers the basics of ScrollBar, but if you’re curious to learn more, you can check out our constitution at the bottom of the page.
-            </Paragraph>
+
+            {events.length > 0 ? (() => {
+              const featuredEvent = events[0];
+              const otherEvents = events.slice(1);
+              return (
+                <div className="eventsGrid">
+                  <div className="featuredEvent">
+                    <Card
+                      hoverable
+                      className="eventCard eventCard--featured"
+                      style={{ backgroundImage: `url(${featuredEvent.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+                    >
+                      <div className="eventOverlay eventOverlay--featured">
+                        <Text className="eventText eventText--date">{featuredEvent.start.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit' })}</Text>
+                        <Text className="eventText eventText--title">{featuredEvent.title}</Text>
+                        <Text className="eventText eventText--description">{EVENT_INFORMATION_LABEL}</Text>
+                      </div>
+                    </Card>
+                  </div>
+                  {otherEvents.map((event) => (
+                    <div key={event.title}>
+                      <Card
+                        hoverable
+                        className="eventCard eventCard--small"
+                        style={{ backgroundImage: `url(${event.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+                      >
+                        <div className="eventOverlay">
+                          <Text className="eventText eventText--date">{event.start.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit' })}</Text>
+                          <Text className="eventText eventText--title">{event.title}</Text>
+                        </div>
+                      </Card>
+                    </div>
+                  ))}
+                </div>
+              );
+            })() : (
+              <Paragraph style={{ textAlign: "center" }}>No upcoming events at the moment.</Paragraph>
+            )}
           </Col>
         </Row>
        <Divider />   
@@ -59,7 +92,4 @@ export default function EventsPage() {
   )
 }
    
-              
             
-
-
