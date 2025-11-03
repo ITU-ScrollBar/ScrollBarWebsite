a1cb17022222144b83450af8af03d50d75c05c4e
A-Guldborg <aguh@itu.dk>
2025-11-03 21:09:42 +0100

    Fix import

--- migrations/002-soft-delete-old-users.ts
+++ migrations/002-soft-delete-old-users.ts
@@ -1,7 +1,6 @@
 import * as Firebase from 'firebase-admin/firestore';
-import { MigrationProps } from './migration-runner';
 
-export default async function ({ db, env }: MigrationProps) {
+export default async function ({ db }: {db: Firebase.Firestore}) {
     const usersRef = db.collection(`users`).where('active', '==', false);
     const snapshot = await usersRef.get();
     const batch = db.batch();
