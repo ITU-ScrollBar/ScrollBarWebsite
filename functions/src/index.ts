414ba4b0de5ba0deaddcb7f7b4ef1488132cf2c6
A-Guldborg <aguh@itu.dk>
2025-12-16 21:48:17 +0100

    Register function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions/v2';
 import calendarApp from './calendar';
 import * as mailservice from './mailservice';
+import { adminChangeUserEmail } from './userManagerService';
 
 // Export the express app as the `calendar` HTTPS function.
 export const calendar = functions.https.onRequest({invoker: "public", region: "europe-west1"}, calendarApp as any);
@@ -8,3 +9,4 @@ export const calendar = functions.https.onRequest({invoker: "public", region: "e
 // Export mail service functions individually so the emulator can detect them.
 export const sendEmailInvite = mailservice.sendEmailInvite;
 export const sendShiftGrabbedConfirmation = mailservice.sendShiftGrabbedConfirmation;
+export const resetUserMail = adminChangeUserEmail;
