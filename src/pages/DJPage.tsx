8f7e46434163e9bac6668229509a829b89d841ae
AnnikaJensby <alyt@itu.dk>
2026-04-26 17:53:31 +0200

    Added DJ's info page with description

--- src/pages/DJPage.tsx
+++ src/pages/DJPage.tsx
@@ -1,16 +1,15 @@
 
 import { Col, Divider, Layout, Row } from 'antd'
-import Title from 'antd/es/typography/Title';
-import Paragraph from 'antd/es/typography/Paragraph';
 import { useWindowSize } from "../hooks/useWindowSize";
+import useSettings from '../hooks/useSettings';
 import HeaderBar from '../components/HomePage/HeaderBar';
-import CountDown from '../components/EventPage/EventCountDown';
-import useEvents from '../hooks/useEvents';
 import DEFAULT_EVENT_IMAGE from '../assets/images/background.png';
+import MDEditor from '@uiw/react-md-editor';
 
 
-export default function EventsPage() {
+export default function DJPage() {
   const { isMobile } = useWindowSize();
+  const { settingsState } = useSettings();
 
   return (
     <Layout 
@@ -22,8 +21,7 @@ export default function EventsPage() {
         height: 'auto',
       }}
     >
-       <HeaderBar />
-
+      <HeaderBar />
       <div 
         style={{
           position: 'relative',
@@ -58,12 +56,45 @@ export default function EventsPage() {
             zIndex: 1,
           }}
         />
-        
 
+        <h1
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: isMobile ? '8%' : '10%',
+            transform: 'translateY(-50%)',
+            color: 'white',
+            zIndex: 2,
+            margin: 0,
+            fontSize: isMobile ? '2.5rem' : '4rem',
+            textAlign: 'left',
+          }}
+        >
+          DJ's in ScrollBar
+        </h1>
       </div>
 
-      <Row justify="center">
-       
+      <Row justify="center" style={{ backgroundColor: '#fff' }}>
+        <Col xs={22} sm={20} md={18} lg={16}>
+          <div
+            style={{
+              padding: '32px 0',
+              width: '100%',
+              color: '#1a1a1a',
+            }}
+          >
+            <MDEditor.Markdown
+              style={{
+                fontSize: '18px',
+                lineHeight: '36px',
+                textAlign: 'left',
+                color: 'black',
+                background: 'white',
+              }}
+              source={settingsState.settings.djdescription || "DJ description will appear here once it is configured in Global Settings."}
+            />
+          </div>
+        </Col>
       </Row>
       
       <Divider />   
